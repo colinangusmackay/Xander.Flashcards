8490c83f17c07bcf60de3e96172f3ce2a3c5a36d
colinangusmackay <colin.mackay@gmail.com>
2014-11-16 23:31:49 +0000

    Set and Clear cookies

--- routes/setLanguage.js
+++ routes/setLanguage.js
@@ -1,4 +1,6 @@
 module.exports = function(req, res){
     var language = req.body.language;
+    var cookieAge = 24*60*60*1000; // 1 day
+    res.cookie("flashcard-language",language,{maxAge:cookieAge, httpOnly:true});
     res.send("set-language to "+language);
 };

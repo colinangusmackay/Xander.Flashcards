483797e2d203d03787352c4d99c40b47b33fea37
colinangusmackay <colin.mackay@gmail.com>
2014-11-18 22:50:41 +0000

    Cookies and Scripts

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ var express = require("express");
 var http = require("http");
 var ejsLayouts = require("express-ejs-layouts");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 
 var welcome = require("./routes/welcome.js");
 var setLanguage = require("./routes/setLanguage.js");
+var flashcard = require("./routes/flashcard.js");
 
 // Set up the application
 var app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("view engine","ejs");
 app.use(ejsLayouts);
+app.set("layout extractScripts", true);
 app.set("views","./views");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.get("/", welcome);
 app.post("/set-language", setLanguage);
+app.get("/flashcard", flashcard);
 
 // Run up the server
 http.createServer(app).listen(app.get("port"), function(){
--- routes/setLanguage.js
+++ routes/setLanguage.js
@@ -2,5 +2,5 @@ module.exports = function(req, res){
     var language = req.body.language;
     var cookieAge = 24*60*60*1000; // 1 day
     res.cookie("flashcard-language",language,{maxAge:cookieAge, httpOnly:true});
-    res.send("set-language to "+language);
+    res.redirect("/flashcard");
 };
